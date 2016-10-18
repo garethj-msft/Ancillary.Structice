58c240bde296649772407d1cff6a13fb5766ef93
gareth@britishbeans.com <gareth@britishbeans.com>
2016-10-18 01:15:06 -0700

    Initial working code.

---
+++ Ancillary.Structice/PriorityQueue.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ancillary.Structice
+{
+    public class PriorityQueue<TKey, TValue> 
+    {
+        const int defaultSize = 20;
+        private IComparer<TKey> comparer;
+        private KeyValuePair<TKey, TValue>[] content = null;
+        private int size = 0;
+
+        public PriorityQueue() : 
+            this(Comparer<TKey>.Default)
+        {
+        }
+
+        public PriorityQueue(IComparer<TKey> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public void Insert(TKey key, TValue value)
+        {
+            if (content == null)
+            {
+                content = new KeyValuePair<TKey, TValue>[defaultSize];
+            }
+            if (content.Length == size)
+            {
+                ReallocateUp();
+            }
+            content[size] = new KeyValuePair<TKey, TValue>(key, value);
+            HeapifyUp(size);
+            size++;
+        }
+
+        public KeyValuePair<TKey, TValue>? Extract()
+        {
+            if (size == 0)
+            {
+                return null;
+            }
+            else if (size == 1)
+            {
+                size = 0;
+                return content[0];
+            }
+            else
+            {
+                KeyValuePair<TKey, TValue> retVal = content[0];
+                size--;
+                content[0] = content[size];
+                HeapifyDown(0);
+                if ((content.Length / 2) - size == 1)
+                {
+                    ReallocateDown();
+                }
+                return retVal;
+            }
+        }
+
+        public KeyValuePair<TKey, TValue>? Peek()
+        {
+            if (size == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return content[0];
+            }
+        }
+
+        public void Clear()
+        {
+            size = 0;
+            content = null;
+        }
+
+        private void HeapifyDown(int location)
+        {
+            int leftChildLocation = LeftChild(location);
+            int rightChildLocation = RightChild(location);
+            int leftComparison = 0;
+            int rightComparison = 0;
+            while (leftChildLocation < size) 
+            {
+                int moveLocation = location;
+                if (leftChildLocation < size && (leftComparison = comparer.Compare(content[leftChildLocation].Key, content[location].Key)) > 0)
+                {
+                    moveLocation = leftChildLocation;
+                }
+                // Be sure to compare with the move location to give priority to the most different.
+                if (rightChildLocation < size && (rightComparison = comparer.Compare(content[rightChildLocation].Key, content[moveLocation].Key)) > 0)
+                {
+                    moveLocation = rightChildLocation;
+                }
+
+                if (moveLocation != location)
+                { 
+                    Swap(location, moveLocation);
+                    location = moveLocation;
+                }
+                else
+                {
+                    break;
+                }
+                leftChildLocation = LeftChild(location);
+                rightChildLocation = RightChild(location);
+            }
+        }
+
+        private void HeapifyUp(int location)
+        {
+            int parentLocation = Parent(location);
+            while (comparer.Compare(content[location].Key, content[parentLocation].Key) > 0)
+            {
+                Swap(location, parentLocation);
+                location = parentLocation;
+                parentLocation = Parent(location); 
+            } 
+        }
+
+        private static int Parent(int location)
+        {
+            return (location - 1) / 2;
+        }
+
+        private static int LeftChild(int location)
+        {
+            return ((location + 1) * 2) - 1;
+        }
+
+        private static int RightChild(int location)
+        {
+            return ((location + 1) * 2);
+        }
+
+        private void Swap(int location, int parentLocation)
+        {
+            KeyValuePair<TKey, TValue> temp = content[parentLocation];
+            content[parentLocation] = content[location];
+            content[location] = temp;
+        }
+
+        private void ReallocateUp()
+        {
+            KeyValuePair<TKey, TValue>[] old = content;
+            content = new KeyValuePair<TKey, TValue>[size * 2];
+            old.CopyTo(content, 0);
+        }
+        private void ReallocateDown()
+        {
+            KeyValuePair<TKey, TValue>[] old = content;
+            int newSize = ((size / defaultSize) + 1) * defaultSize;
+            if (newSize != content.Length)
+            {
+                content = new KeyValuePair<TKey, TValue>[newSize];
+                Array.Copy(old, content, newSize);
+            }
+        }
+
+#if DEBUG
+        public string Dump()
+        {
+            int count = 0;
+            int lineCount = 0;
+            int rowCount = 0;
+            string dump = string.Empty;
+            while (count < size)
+            {
+                if ((count+1) == (Math.Pow(2,rowCount)))
+                {
+                    if (rowCount != 0)
+                    {
+                        dump += Environment.NewLine;
+                    }
+                    rowCount++;
+                    lineCount = 0;
+                }
+                if (lineCount != 0)
+                {
+                    dump += ", ";
+                }
+                var p = content[count];
+                dump += p.Key + ":" + p.Value;
+                count++;
+                lineCount++;
+            }
+            return dump;
+        }
+#endif
+    }
+}
---
+++ Test.Ancillary.Structice/TestPriorityQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ancillary.Structice;
+using System.Collections.Generic;
+
+namespace Test.Ancillary.Structice
+{
+    [TestClass]
+    public class TestPriorityQueue
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void RandomInsertCheckOrderedExtract()
+        {
+            var queue = new PriorityQueue<int, string>();
+            queue.Insert(100, "One Hundred");
+            queue.Insert(1, "One");
+            queue.Insert(5, "Five");
+            queue.Insert(75, "Seventy Five");
+            queue.Insert(32, "Thirty Two");
+            queue.Insert(64, "Sixty Four");
+            queue.Insert(18, "Eighteen");
+            TestContext.WriteLine(@"Initial: " + queue.Dump());
+            TestContext.WriteLine("");
+
+            KeyValuePair<int, string>? value;
+            int priority = Int32.MaxValue;
+            while ( (value = queue.Extract()).HasValue)
+            {
+                TestContext.WriteLine(value.Value.Value);
+                TestContext.WriteLine(@"    " + queue.Dump());
+                TestContext.WriteLine("");
+                Assert.IsTrue(value.Value.Key <= priority, "Priority violation.");
+                priority = value.Value.Key;
+            }
+        }
+    }
+}
