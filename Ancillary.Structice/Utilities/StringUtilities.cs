923d69f3faa66ea3325d26942258bc399b2576c1
Gareth Jones <garethj@microsoft.com>
2017-06-23 02:38:46 -0700

    Aded StringUtilities.

---
+++ Ancillary.Structice/Utilities/StringUtilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancillary.Structice.Utilities
+{
+    public static class StringUtilities
+    {
+
+        public static IEnumerable<IGrouping<string, string>> ThenSplit(this string[] theList, char separator)
+        {
+            foreach (string entry in theList)
+            {
+                yield return new GroupedEnumerable(entry, entry.Split(separator));
+            }
+        }
+
+        internal class GroupedEnumerable : IGrouping<string, string>
+        {
+            public string Key { get; private set; }
+
+            private string[] Values { get; set; }
+
+            internal GroupedEnumerable(string key, string[] values)
+            {
+                this.Key = key;
+                this.Values =  values;
+            }
+
+            public IEnumerator<string> GetEnumerator()
+            {
+                return ((IEnumerable<string>)this.Values).GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.Values.GetEnumerator();
+            }
+        }
+    }
+}
