5bf60e5a3a543f48afcf999be20cb0a3e3972f42
Gareth Jones <garethj@microsoft.com>
2020-03-03 04:44:16 -0800

    Switched to local methods.

--- Ancillary.Structice/PriorityQueue.cs
+++ Ancillary.Structice/PriorityQueue.cs
@@ -56,13 +56,34 @@ namespace Ancillary.Structice
                 this.content = new (TPriority, TValue)[DefaultSize];
             }
 
+            // Local functions
+            int parent(int location) => (location - 1) / 2;
+
+            void reallocateUp()
+            {
+                (TPriority priority, TValue value)[] old = this.content;
+                this.content = new (TPriority priority, TValue value)[this.Count * 2];
+                old.CopyTo(this.content, 0);
+            }
+
+            void heapifyUp(int location)
+            {
+                int parentLocation = parent(location);
+                while (this.content[location].priority.CompareTo(this.content[parentLocation].priority) > 0)
+                {
+                    this.Swap(location, parentLocation);
+                    location = parentLocation;
+                    parentLocation = parent(location);
+                }
+            }
+
             if (this.content.Length == this.Count)
             {
-                this.ReallocateUp();
+                reallocateUp();
             }
 
             this.content[this.Count] = (priority, value);
-            this.HeapifyUp(this.Count);
+            heapifyUp(this.Count);
             this.Count++;
         }
 
@@ -72,27 +93,75 @@ namespace Ancillary.Structice
         /// <returns>The entry.</returns>
         public (TPriority priority, TValue value)? Dequeue()
         {
-            if (this.Count == 0)
-            {
-                return null;
-            }
-            else if (this.Count == 1)
-            {
-                this.Count = 0;
-                return this.content[0];
-            }
-            else
-            {
-                (TPriority, TValue) retVal = this.content[0];
-                this.Count--;
-                this.content[0] = this.content[this.Count];
-                this.HeapifyDown(0);
-                if ((this.content.Length / 2) - this.Count == 1)
-                {
-                    this.ReallocateDown();
-                }
+            // Local functions
+            int leftChild(int location) => ((location + 1) * 2) - 1;
 
-                return retVal;
+            int rightChild(int location) => (location + 1) * 2;
+
+            void reallocateDown()
+            {
+                (TPriority priority, TValue value)[] old = this.content;
+                int newSize = ((this.Count / DefaultSize) + 1) * DefaultSize;
+                if (newSize != this.content.Length)
+                {
+                    this.content = new (TPriority priority, TValue value)[newSize];
+                    Array.Copy(old, this.content, newSize);
+                }
+            }
+
+            void heapifyDown(int location)
+            {
+                int leftChildLocation = leftChild(location);
+                int rightChildLocation = rightChild(location);
+                while (leftChildLocation < this.Count)
+                {
+                    int moveLocation = location;
+                    if (leftChildLocation < this.Count && this.content[leftChildLocation].priority.CompareTo(this.content[location].priority) > 0)
+                    {
+                        moveLocation = leftChildLocation;
+                    }
+
+                    // Be sure to compare with the move location to give priority to the most different.
+                    if (rightChildLocation < this.Count && this.content[rightChildLocation].priority.CompareTo(this.content[moveLocation].priority) > 0)
+                    {
+                        moveLocation = rightChildLocation;
+                    }
+
+                    if (moveLocation != location)
+                    {
+                        this.Swap(location, moveLocation);
+                        location = moveLocation;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    leftChildLocation = leftChild(location);
+                    rightChildLocation = rightChild(location);
+                }
+            }
+
+            switch (this.Count)
+            {
+                case 0:
+                    return null;
+                case 1:
+                    this.Count = 0;
+                    return this.content[0];
+                default:
+                {
+                    (TPriority, TValue) retVal = this.content[0];
+                    this.Count--;
+                    this.content[0] = this.content[this.Count];
+                    heapifyDown(0);
+                    if ((this.content.Length / 2) - this.Count == 1)
+                    {
+                        reallocateDown();
+                    }
+
+                    return retVal;
+                }
             }
         }
 
@@ -150,7 +219,7 @@ namespace Ancillary.Structice
                     dump.Append(", ");
                 }
 
-                var (priority, value) = this.content[count];
+                (TPriority priority, TValue value) = this.content[count];
                 dump.Append($"{priority}:{value}");
                 count++;
                 lineCount++;
@@ -160,79 +229,16 @@ namespace Ancillary.Structice
         }
 #endif
 
-        private static int Parent(int location) => (location - 1) / 2;
-
-        private static int LeftChild(int location) => ((location + 1) * 2) - 1;
-
-        private static int RightChild(int location) => (location + 1) * 2;
-
-        private void HeapifyDown(int location)
+        /// <summary>
+        /// Swap two elements withing the content array.
+        /// </summary>
+        /// <param name="firstLocation">Location of first element to swap.</param>
+        /// <param name="secondLocation">Location of second element to swap.</param>
+        private void Swap(int firstLocation, int secondLocation)
         {
-            int leftChildLocation = LeftChild(location);
-            int rightChildLocation = RightChild(location);
-            while (leftChildLocation < this.Count)
-            {
-                int moveLocation = location;
-                if (leftChildLocation < this.Count && this.content[leftChildLocation].priority.CompareTo(this.content[location].priority) > 0)
-                {
-                    moveLocation = leftChildLocation;
-                }
-
-                // Be sure to compare with the move location to give priority to the most different.
-                if (rightChildLocation < this.Count && this.content[rightChildLocation].priority.CompareTo(this.content[moveLocation].priority) > 0)
-                {
-                    moveLocation = rightChildLocation;
-                }
-
-                if (moveLocation != location)
-                {
-                    this.Swap(location, moveLocation);
-                    location = moveLocation;
-                }
-                else
-                {
-                    break;
-                }
-
-                leftChildLocation = LeftChild(location);
-                rightChildLocation = RightChild(location);
-            }
-        }
-
-        private void HeapifyUp(int location)
-        {
-            int parentLocation = Parent(location);
-            while (this.content[location].priority.CompareTo(this.content[parentLocation].priority) > 0)
-            {
-                this.Swap(location, parentLocation);
-                location = parentLocation;
-                parentLocation = Parent(location);
-            }
-        }
-
-        private void Swap(int location, int parentLocation)
-        {
-            (TPriority, TValue) temp = this.content[parentLocation];
-            this.content[parentLocation] = this.content[location];
-            this.content[location] = temp;
-        }
-
-        private void ReallocateUp()
-        {
-            (TPriority priority, TValue value)[] old = this.content;
-            this.content = new (TPriority priority, TValue value)[this.Count * 2];
-            old.CopyTo(this.content, 0);
-        }
-
-        private void ReallocateDown()
-        {
-            (TPriority priority, TValue value)[] old = this.content;
-            int newSize = ((this.Count / DefaultSize) + 1) * DefaultSize;
-            if (newSize != this.content.Length)
-            {
-                this.content = new (TPriority priority, TValue value)[newSize];
-                Array.Copy(old, this.content, newSize);
-            }
+            (TPriority, TValue) temp = this.content[secondLocation];
+            this.content[secondLocation] = this.content[firstLocation];
+            this.content[firstLocation] = temp;
         }
     }
 }
